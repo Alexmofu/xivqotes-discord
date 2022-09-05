5e0fe15bc65d8267f53eba2d3c975df996a1f9c7
Alexmofu <Alexmofu98@gmail.com>
2022-09-05 05:38:09 +0200

    Last commit changes reverted

--- src/index.js
+++ src/index.js
@@ -20,10 +20,6 @@ const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
 
 //  Client on ready
 client.on('ready', () => console.log(`Connected ${client.user.tag}`));
-client.on('ready', () => {
-    client.guilds.fetch("1010180411525173349")
-	.then(guild => guild.leave())
-})
 
 //  Client interactions
 client.on('interactionCreate', async interaction => {
